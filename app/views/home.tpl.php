c20409e5eeebee6077d406f85fec9f25f160416f
YanBerdin <yandevformation@gmail.com>
2023-08-04 01:07:29 +0200

    Fixed Homepage  asset's file path

--- app/views/home.tpl.php
+++ app/views/home.tpl.php
@@ -1,8 +1,9 @@
+<?php // var_dump($absoluteURL); ?>
 <section>
     <div class="container-fluid">
         <div class="row mx-0">
             <div class="col-md-6">
-                <div class="card border-0 text-white text-center"><img src="<?= $absoluteURL ?>assets/images/categ5.jpeg" alt="Card image" class="card-img">
+                <div class="card border-0 text-white text-center"><img src="<?= $absoluteURL ?>/assets/images/categ5.jpeg" alt="Card image" class="card-img">
                     <div class="card-img-overlay d-flex align-items-center">
                         <div class="w-100 py-3">
                             <h2 class="display-3 font-weight-bold mb-4">Vintage</h2><a href="products_list.html" class="btn btn-light">Découvrir</a>
@@ -11,7 +12,7 @@
                 </div>
             </div>
             <div class="col-md-6">
-                <div class="card border-0 text-white text-center"><img src="<?= $absoluteURL ?>assets/images/categ2.jpeg" alt="Card image" class="card-img">
+                <div class="card border-0 text-white text-center"><img src="<?= $absoluteURL ?>/assets/images/categ2.jpeg" alt="Card image" class="card-img">
                     <div class="card-img-overlay d-flex align-items-center">
                         <div class="w-100 py-3">
                             <h2 class="display-3 font-weight-bold mb-4">Au travail</h2><a href="products_list.html" class="btn btn-light">C'est parti</a>
@@ -23,7 +24,7 @@
         <div class="row mx-0">
 
             <div class="col-lg-4">
-                <div class="card border-0 text-center text-white"><img src="<?= $absoluteURL ?>assets/images/categ4.jpeg" alt="Card image" class="card-img">
+                <div class="card border-0 text-center text-white"><img src="<?= $absoluteURL ?>/assets/images/categ4.jpeg" alt="Card image" class="card-img">
                     <div class="card-img-overlay d-flex align-items-center">
                         <div class="w-100">
                             <h2 class="display-4 mb-4">Sortir</h2><a href="products_list.html" class="btn btn-link text-white">Faire un tour
@@ -34,7 +35,7 @@
             </div>
             <div class="col-lg-4">
                 <div class="card border-0 text-center text-dark">
-                    <img src="<?= $absoluteURL ?>assets/images/categ1.jpeg" alt="Card image" class="card-img">
+                    <img src="<?= $absoluteURL ?>/assets/images/categ1.jpeg" alt="Card image" class="card-img">
                     <div class="card-img-overlay d-flex align-items-center">
                         <div class="w-100">
                             <h2 class="display-4 mb-4">Détente</h2>
@@ -46,7 +47,7 @@
                 </div>
             </div>
             <div class="col-lg-4">
-                <div class="card border-0 text-center text-white"><img src="<?= $absoluteURL ?>assets/images/categ3.jpeg" alt="Card image" class="card-img">
+                <div class="card border-0 text-center text-white"><img src="<?= $absoluteURL ?>/assets/images/categ3.jpeg" alt="Card image" class="card-img">
                     <div class="card-img-overlay d-flex align-items-center">
                         <div class="w-100">
                             <h2 class="display-4 mb-4">Cérémonie</h2><a href="products_list.html" class="btn btn-link text-white">Bien choisir <i class="fa-arrow-right fa ml-2"></i></a>
