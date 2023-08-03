f12415b6a7bd053ab4e70e5645059b9a5ab62be0
YanBerdin <yandevformation@gmail.com>
2023-08-03 21:16:08 +0200

    ErrorController Done

--- app/controllers/ErrorController.php
+++ app/controllers/ErrorController.php
@@ -1,26 +1,29 @@
 <?php
+// Heritage
+require_once __DIR__ . '/CoreController.php';
 
-class ErrorController
+class ErrorController extends CoreController
 {
     public function error404()
     {
         $this->show("error404");
     }
 
+    // Commenté => Maintenant c'est CoreModel qui déclare show()
     /**
      * Affiche la page
      *
      * @param string $viewName
      * @param array $viewData (Facultatif)
      */
-    public function show($viewName, $viewData = [])
-    {
-        // global $router; // Ca c'est hyper degueulasse mais pour l'instant ça fait le café
+    // public function show($viewName, $viewData = [])
+    // {
+    // global $router; // Ca c'est hyper degueulasse mais pour l'instant ça fait le café
 
-        $baseUrl = $_SERVER['BASE_URI'] . '/';
+    //     $baseUrl = $_SERVER['BASE_URI'] . '/';
 
-        require_once __DIR__ . '/../views/header.tpl.php';
-        require_once __DIR__ . '/../views/' . $viewName . '.tpl.php';
-        require_once __DIR__ . '/../views/footer.tpl.php';
-    }
+    //     require_once __DIR__ . '/../views/header.tpl.php';
+    //     require_once __DIR__ . '/../views/' . $viewName . '.tpl.php';
+    //     require_once __DIR__ . '/../views/footer.tpl.php';
+    // }
 }
